011ae201a2681a592ac0f56f05348bf2ca5927c6
pijoules <lchan1994@yahoo.com>
2016-12-19 00:35:22 -0500

    gotta increase that github score

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 #import random
 
 def test():
-    if random.random() < 0.5:
-        return 1 # type: float
-    return "a"
+    if random.random() < 0.3:
+        return 1
+    elif random.random() < 0.6:
+        return "a"
+    else:
+        return {"a": 2}
 
 print(test())
