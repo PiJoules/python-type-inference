e72c98e642d445fd73e3fb251e76102a5c3464fc
pijoules <lchan1994@yahoo.com>
2017-04-14 22:57:58 -0400

    gotta increase that github score

--- metaclass_type.py
+++ metaclass_type.py
@@ -7,8 +7,43 @@ class MetaclassType(PyType):
         Args:
             cls_type: The class type this meta type returns
         """
+        from class_type import ClassType
         super().__init__("type", builtins, parents=[self], **kwargs)
         self.__cls_type = cls_type
+        assert isinstance(self.__cls_type, ClassType)
+
+    def inst(self, *args, **kwargs):
+        return self.__cls_type
 
     def call(self, args):
+        cls_inst = self.inst()
+        cls_inst.call_new(args)
+        return {cls_inst}
+
+    def __hash__(self):
+        return id(self)
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+
+
+
+class ClassType(PyType):
+    def __init__(self, builtins, inst_type, **kwargs):
+        super().__init__("type", builtins, **kwargs)
+
+
+import unittest
+
+
+class TestMetaClassType(unittest.TestCase):
+    def setUp(self):
         pass
+
+    def test_creation(self):
+        """Test we can create a metaclass."""
+
+
+if __name__ == "__main__":
+    unittest.main()
